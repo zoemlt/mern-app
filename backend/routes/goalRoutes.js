869e5bf300e171371866860b5b2414b2315553e8
zoe <zoejniu@gmail.com>
2022-02-12 13:08:58 +1300

    Create and connect controllers to routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,14 @@
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get goals" });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Set goal" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-});
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
 
 module.exports = router;

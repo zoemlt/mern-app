f3bdc090bb1431f3ad3c0a470ec21fca196e4093
zoe <zoejniu@gmail.com>
2022-02-18 13:45:04 +1300

    Use JWT authentication and middleware to protect user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,6 +25,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -43,6 +44,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -54,7 +56,13 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/me
 // @access  public
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: "User data display" });
+  const { _id, name, email } = await User.findById(req.user.id);
+  res.status(200).json({ id: _id, name, email });
 });
+
+// Generate JWT
+const generateToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
 
 module.exports = { registerUser, loginUser, getMe };
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+      // Verify token and get user
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (err) {
+      console.log(err);
+      res.status(401);
+      throw new Error("You are not authorized");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("No authorization token");
+  }
+});
+
+module.exports = { protect };
